1e6a06f71fd00d09c36cdd2c38a2c0f1541dbc02
Pravesh-mansharamani <praveshramani295@gmail.com>
2024-02-01 01:47:45 -0500

    better sol

--- neetcode150/arrays/validAnagram.py
+++ neetcode150/arrays/validAnagram.py
@@ -7,4 +7,23 @@ class Solution:
     But this more easier and techinally both are O(n)
     '''
 
+    # solution for interviews since above one is one line :O
+    def isAnagram1(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        hashmapS = {}
+        hashmapT = {}
+
+        # making the hashmaps:
+        for i in range(len(s)):
+            hashmapS[s[i]] = 1 + hashmapS.get(s[i], 0)
+            hashmapT[t[i]] = 1 + hashmapT.get(t[i], 0)
+
+        # now checking if they are equal    
+        for i in hashmapS:
+            if hashmapS[i] != hashmapT.get(i, 0):
+                return False
+        
+        return True
     
